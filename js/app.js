86842fd1b0697a76915cce276ab9a6e94479b205
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-09-24 10:52:30 +0330

    change style selected button

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
 
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
+
 const searchHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
 
@@ -17,13 +27,12 @@ const searchHandler = (event) => {
 
 const filterHandler = (event) => {
   const filter = event.target.dataset.filter;
-
+  changeClass(filter);
   products.forEach((product) => {
     const category = product.dataset.category;
     if (filter === "all") {
       product.style.display = "block";
     } else {
-      
       filter === category
         ? (product.style.display = "block")
         : (product.style.display = "none");
